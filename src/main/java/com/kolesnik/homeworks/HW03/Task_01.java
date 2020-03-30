9433145bd819db58bbb7a14ac02d0868a639bff6
SerheyKolesnik <sergey.kolesnik.dnepr@gmail.com>
2020-03-30 17:39:10 +0300

    HW03

--- src/main/java/com/kolesnik/homeworks/HW03/Task_01.java
+++ src/main/java/com/kolesnik/homeworks/HW03/Task_01.java
@@ -5,6 +5,7 @@ he drinks 0.5 litres of water per hour of cycling.
 You get given the time in hours and you need to return
 the number of litres Nathan will drink, rounded to the
 smallest value.
+
 For example:
 time = 3 ----> litres = 1
 time = 6.7---> litres = 3
--- src/main/java/com/kolesnik/homeworks/HW03/Task_04.java
+++ src/main/java/com/kolesnik/homeworks/HW03/Task_04.java
@@ -2,10 +2,8 @@
 You are given a function describeAge that takes a parameter age (which
 will always be a positive integer) and does the following:
 If the age is 12 or lower, it return "You're a(n) kid"
-If the age is anything between 13 and 17 (inclusive), it return "You're a(n)
-teenager"
-If the age is anything between 18 and 64 (inclusive), it return "You're a(n)
-adult"
+If the age is anything between 13 and 17 (inclusive), it return "You're a(n)teenager"
+If the age is anything between 18 and 64 (inclusive), it return "You're a(n)adult"
 If the age is 65 or above, it return "You're a(n) elderly"
 Your task is to shorten the code as much as possible
  */
