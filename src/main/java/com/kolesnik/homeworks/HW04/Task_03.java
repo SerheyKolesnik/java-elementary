dc0f899c3d1d61d3d1c8901ba7c6c755af8e082f
SerheyKolesnik <sergey.kolesnik.dnepr@gmail.com>
2020-04-02 18:20:38 +0300

    kolesnik.HW04

---
+++ src/main/java/com/kolesnik/homeworks/HW04/Task_03.java
@@ -0,0 +1,42 @@
+/*
+Write a program which calculates the sum of two columns.
+Print table with results if columns have the same size (col_1| col_2 | sum).
+Otherwise print which column has bigger size
+ */
+package com.kolesnik.homeworks.HW04;
+
+import java.util.Random;
+
+public class Task_03 {
+    public static void main(String[] args) {
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+        fillArray(arr1);
+        fillArray(arr2);
+        compareArr(arr1, arr2);
+    }
+
+    private static void fillArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = RandomNum(10);
+        }
+    }
+
+    private static int RandomNum(int span_) {
+        Random r = new Random();
+        return r.nextInt(span_);
+    }
+
+    private static void compareArr(int[] arr1, int[] arr2) {
+        if (arr1.length > arr2.length) {
+            System.out.println("column 1 has bigger size");
+        } else if (arr1.length < arr2.length) {
+            System.out.println("column 2 has bigger size");
+        } else {
+            System.out.println("(col_1| col_2 | sum)");
+            for (int i = 0; i < arr1.length; i++) {
+                System.out.printf("(%s | %s | %s)\n", arr1[i], arr2[i], arr1[i] + arr2[i]);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/kolesnik/homeworks/HW04/Task_04.java
@@ -0,0 +1,42 @@
+/*
+• * Write a program which checks if given number is
+STRONG, i.e. the sum of the factorials of its digits is equal
+to number itself: 1! + 4! + 5! = 1 + 24 + 120 = 145
+ */
+package com.kolesnik.homeworks.HW04;
+
+public class Task_04 {
+    public static void main(String[] args) {
+        int inputNum = 145;
+        System.out.printf("Number %s is STRONG: %s", inputNum, checkStrongNumber(inputNum));
+    }
+
+    private static int calculateFactorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    private static int[] numArr(int number_) {
+        String n = Integer.toString(number_);
+        char[] charArray = n.toCharArray();
+        int[] arr = new int[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+            arr[i] = Integer.parseInt(String.valueOf(charArray[i]));
+        }
+        return arr;
+    }
+
+    private static boolean checkStrongNumber(int number_) {
+        int sumFactorialsDigit = 0;
+        for (int i = 0; i < numArr(number_).length; i++) {
+            sumFactorialsDigit += calculateFactorial(numArr(number_)[i]);
+        }
+        if (sumFactorialsDigit == number_) {
+            return true;
+        }
+        return false;
+    }
+}
